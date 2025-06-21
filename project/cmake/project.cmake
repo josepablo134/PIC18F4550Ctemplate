963c0fc0e83259e508bdfd8ba8cb9ce22af69374
Josepablo C <jcruzbaas@gmail.com>
2025-06-21 15:42:05 -0600

    fix(cmake): linux support

--- project/cmake/project.cmake
+++ project/cmake/project.cmake
@@ -9,10 +9,19 @@ set( CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/libs )
 set( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin )
 set( CMAKE_EXECUTABLE_SUFFIX .elf)
 
-# Referenced from: https://github.com/AymenLA/LedBlinker.X
-set( MPLABX_BASE_PATH "C:/Program Files/Microchip/MPLABX/v6.20/" CACHE STRING "MPLAB X")
-set( MPLABX_JAVA "${MPLABX_BASE_PATH}/sys/java/zulu8.64.0.19-ca-fx-jre8.0.345-win_x64/bin/java" CACHE STRING "MPLAB X Java")
-set( MPLABX_IPE_CLI "${MPLABX_BASE_PATH}/mplab_platform/mplab_ipe/ipecmd.jar" CACHE STRING "MPLAB X IPE")
+if(WIN32)
+    # Referenced from: https://github.com/AymenLA/LedBlinker.X
+	message("Configuring for win")
+    set( MPLABX_BASE_PATH "C:/Program Files/Microchip/MPLABX/v6.20/" CACHE STRING "MPLAB X")
+    set( MPLABX_JAVA "${MPLABX_BASE_PATH}/sys/java/zulu8.64.0.19-ca-fx-jre8.0.345-win_x64/bin/java" CACHE STRING "MPLAB X Java")
+    set( MPLABX_IPE_CLI "${MPLABX_BASE_PATH}/mplab_platform/mplab_ipe/ipecmd.jar" CACHE STRING "MPLAB X IPE")
+else()
+    # Referenced from: https://github.com/AymenLA/LedBlinker.X
+	message("Configuring for linux")
+    set( MPLABX_BASE_PATH "/opt/microchip/mplabx/v6.25" CACHE STRING "MPLAB X")
+    set( MPLABX_JAVA "${MPLABX_BASE_PATH}/sys/java/zulu8.80.0.17-ca-fx-jre8.0.422-linux_x64/bin/java" CACHE STRING "MPLAB X Java")
+    set( MPLABX_IPE_CLI "${MPLABX_BASE_PATH}/mplab_platform/mplab_ipe/ipecmd.jar" CACHE STRING "MPLAB X IPE")
+endif()
 
 set( MCU_DEVICE "18F4550" )
 # tool selection could be done by 
