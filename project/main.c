c06fb836400c437d01c54ab54f7156a9c987daff
Josepablo C <jcruzbaas@gmail.com>
2025-06-21 20:03:43 -0600

    adding a counter to the UART message

--- project/main.c
+++ project/main.c
@@ -1,19 +1,25 @@
+#include "stdio.h"
 #include "UART.h"
 
-char msg[] = "Hello world!!\n";
+char msg[] = "Hello world!! [256]   \n";
 
 void main(void) {
     Board_Init();
     UART_Init();
     UART_Open( 115200U );
 
+    uint8_t counter = 0U;
+
     while(1){
-        LATAbits.LA4 = !PORTAbits.RA4;
+        snprintf( msg, sizeof(msg), "Hello world!! [%u]\n", counter );
+
         while( UART_Status() ){}
         UART_TransmitAsync( 
             (const uart_byte*) msg,
             sizeof( msg ) );
-        /* Do something else */
+
+            counter ++;
+        LATAbits.LA4 = !PORTAbits.RA4;
         __delay_ms( 100 );
     }
 }
